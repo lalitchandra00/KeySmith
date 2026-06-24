5616bcc73e181b945f5b1ae6d2b111f60e718015
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-06-24 23:32:35 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState , useCallback} from 'react'
+
+
+
+
+import { useState , useCallback, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8);
+  const [Length, setLength] = useState(8);
   const [NumAllowed, setNumAllowed] = useState(false);
   const [CharAllowed, setCharAllowed] = useState(false);
   const [Password, setPassword] = useState('');
@@ -11,7 +15,7 @@ function App() {
     let pass = '';
     let str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     
-    if (NumAllowerd){
+    if (NumAllowed){
       str = str + "0123456789"
     }
 
@@ -19,10 +23,10 @@ function App() {
       str = str + "!@#$%^&*()_+=[]{}|:;'?><,."
     }
 
-    for (let i = 1; i <= length; i++) {
+    for (let i = 1; i <= Length; i++) {
 
       // Generate a random index number from 1 to the length of the character string.
-      let index = Math.floor(Math.random() * str.length +1);
+      let index = Math.floor(Math.random() * (str.length + 1));
       // Append the character at the random index to the password.
       pass = pass + str.charAt(index);
     }
@@ -31,8 +35,9 @@ function App() {
   }
 
 
-  const password = useCallback(genpass,[length, NumAllowed, CharAllowed, setPassword])
+  const password = useCallback(genpass,[Length, NumAllowed, CharAllowed, setPassword])
 
+  useEffect(()=>{password()}, [Length, NumAllowed, CharAllowed, password])
 
   return (
     <>
@@ -44,7 +49,7 @@ function App() {
         <br />
         <div className = "flex">
           <input type="text"
-            value = {password}
+            value = {Password}
             className = "rounded-l-2xl bg-amber-200  w-130 h-10 flex justify-center mx-auto p-3 mr-0"
             placeholder = "password"
             readOnly
@@ -52,14 +57,34 @@ function App() {
           <button className = "bg-blue-400 rounded-r-2xl p-2">Copy</button>
         </div>
         <br />
-        <div className = "flex">
-          <input 
-          type="range"
-          min = {8}
-          max = {20}
-          value = {length}
-          onChange = 
-           />
+        <div className = "flex justify-between">
+          <span>
+            <input 
+              type="range"
+              min = {8}
+              max = {20}
+              value = {Length}
+              onChange = {(e) => {setLength(e.target.value)}}
+            />
+              
+            <label className = "ml-1.5 mr-6 text-white">Length: {Length}</label>
+          </span>
+          
+          <span>
+            <input type="checkbox"
+              defaultChecked = {NumAllowed}
+              onChange = {() => {setNumAllowed((prev)=> (!prev))}}
+            />
+            <label className = "ml-1.5 mr-6 text-white">Numbers</label>
+          </span>
+
+         <span>
+           <input type="checkbox"
+            defaultChecked = {CharAllowed}
+            onChange = {() => {setCharAllowed((prev)=> (!prev))}}
+          />
+          <label className = "ml-1.5 mr-6 text-white">Characters</label>
+         </span>
         </div>
       </div>
     </div>
